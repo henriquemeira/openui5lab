d69d25fa15242d5f1826ba1c2123f8d8a9866f53
Henrique Meira <henrique@h2asol.com>
2018-11-28 16:59:19 -0200

    corrige metodo

--- walkthrough/controller/App.controller.js
+++ walkthrough/controller/App.controller.js
@@ -24,8 +24,8 @@ sap.ui.define([
         },
         onShowHello: function() {
             // read msg from i18n model
-            var oBundle = this.getView.getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView.getModel().getProperty("/recipient/name");
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
             var sMsg = oBundle.getText("helloMsg", [sRecipient]);
             // show message
             MessageToast.show(sMsg);
