b10717918aeb0bd3903a380c92b6ca389fdb0561
Henrique Meira <henrique@h2asol.com>
2018-12-07 15:20:26 -0200

    Step 37: Content Density

--- walkthrough/Component.js
+++ walkthrough/Component.js
@@ -45,6 +45,18 @@ sap.ui.define([
 
         openHelloDialog : function() {
             this._helloDialog.open();
-        }
+        },
+        
+        getContentDensityClass : function() {
+			if (!this._sContentDensityClass) {
+				if (!sap.ui.Device.support.touch) {
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
+		}
+
     });
 });
