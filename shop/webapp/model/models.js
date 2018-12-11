ae9c015171e08192e9f98016d640bef1280bffa4
Henrique Meira <henrique@h2asol.com>
2018-12-11 14:55:01 -0200

    Step 7: Adding a Comments Section

--- shop/webapp/model/models.js
+++ shop/webapp/model/models.js
@@ -10,6 +10,9 @@ sap.ui.define([
 			var oModel = new JSONModel(Device);
 			oModel.setDefaultBindingMode("OneWay");
 			return oModel;
+		},   
+		createCommentsModel: function() {
+		   return new JSONModel({ productComments : [] });
 		}
 	};
 
