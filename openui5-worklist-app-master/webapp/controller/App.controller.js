e78ccb1eb943aa5b9faebd4a927bd8f9e0dfb3f9
Henrique Meira <henrique@meira.net>
2019-02-07 11:21:52 -0200

    Teste de migracao de aplicacao

--- openui5-worklist-app-master/webapp/controller/App.controller.js
+++ openui5-worklist-app-master/webapp/controller/App.controller.js
@@ -7,6 +7,9 @@ sap.ui.define([
 	return BaseController.extend("sap.ui.demo.worklist.controller.App", {
 
 		onInit : function () {
+			this.aSearchFilters = [];
+			this.aTabFilters = [];
+			/*
 			var oViewModel,
 				fnSetAppNotBusy,
 				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
@@ -23,12 +26,12 @@ sap.ui.define([
 			};
 
 			// disable busy indication when the metadata is loaded and in case of errors
-			this.getOwnerComponent().getModel().metadataLoaded().
-				then(fnSetAppNotBusy);
+			this.getOwnerComponent().getModel().metadataLoaded().then(fnSetAppNotBusy);
 			this.getOwnerComponent().getModel().attachMetadataFailed(fnSetAppNotBusy);
 
 			// apply content density mode to root view
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+			*/
 		}
 	});
 
