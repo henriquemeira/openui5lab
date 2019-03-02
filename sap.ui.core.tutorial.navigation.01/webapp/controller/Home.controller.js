92b364d0e5921425f8f2be8accd32557a0bb280c
Henrique Meira <henrique@meira.net>
2019-03-01 22:04:57 -0300

    Concluido Step 5: Display a Target Without Changing the Hash

--- sap.ui.core.tutorial.navigation.01/webapp/controller/Home.controller.js
+++ sap.ui.core.tutorial.navigation.01/webapp/controller/Home.controller.js
@@ -4,6 +4,13 @@ sap.ui.define([
     "use strict";
  
     return BaseController.extend("sap.ui.demo.nav.controller.Home", {
+
+      onDisplayNotFound : function () {
+         //display the "notFound" target without changing the hash
+			this.getRouter().getTargets().display("notFound", {
+				fromTarget : "home"
+			});
+		}
  
     });
  
