9808d3f26cc1839b4dc5d78d25c0bae0df59d9a6
Henrique Meira <henrique@meira.net>
2019-03-06 12:40:43 -0300

    testes de navegacao

--- sap.ui.core.tutorial.navigation.01/webapp/controller/employee/EmployeeList.controller.js
+++ sap.ui.core.tutorial.navigation.01/webapp/controller/employee/EmployeeList.controller.js
@@ -4,6 +4,15 @@ sap.ui.define([
 	"use strict";
 	return BaseController.extend("sap.ui.demo.nav.controller.employee.EmployeeList", {
         onListItemPressed : function(oEvent){
+
+			console.log("-------------------------------------------")
+			console.log("oEvent.getSource(): " + oEvent.getSource());
+			console.log(oEvent.getSource());
+			console.log("-------------------------------------------")
+			console.log("oEvent.getSource().getBindingContext(): " + oEvent.getSource().getBindingContext());
+			console.log(oEvent.getSource().getBindingContext());
+			console.log("-------------------------------------------");
+			
 			var oItem, oCtx;
 			oItem = oEvent.getSource();
 			oCtx = oItem.getBindingContext();
